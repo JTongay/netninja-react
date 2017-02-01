880ad8e32ccca3c560f463e70f032a1f3fead061
Joseph Tongay <dirk.tongay@gmail.com>
2017-01-31 20:52:09 -0600

    very basic use of props

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Most of the React code comprises of components
  - To-do list component
   - List item component
   - Add new item (form) component
+
+Props
+  - They are a way to pass data into the components which we want to display on the page.
+  - Dynamic Components
--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,25 @@
-var React = require('react');
-var ReactDOM = require('react-dom')
-var PropTypes = React.PropTypes;
+import React, { PropTypes } from 'react'
+import ReactDOM from 'react-dom'
 
-var TodoComponent = React.createClass({
-
-  render: function() {
+const TodoComponent = React.createClass({
+  render () {
     return (
-      <h1>Booyah</h1>
-    );
+      <div>
+        <p><strong>Cheese Name:</strong> {this.props.cheese.name}</p>
+        <p><strong>Cheese Smell Factor:</strong> {this.props.cheese.smellFactor}</p>
+        <p><strong>Cheese Price:</strong> {this.props.cheese.price}</p>
+
+      </div>
+    )
   }
+})
 
-});
+const myCheese = {
+  name: 'Gouda',
+  smellFactor: 'Extreme pong',
+  price: '3.50'
+}
 
-//put component into html page
-ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'))
+ReactDOM.render(<TodoComponent mssg="i like cheese" cheese={myCheese}/>, document.getElementById('todo-wrapper'))
 
-module.exports = TodoComponent;
+export default TodoComponent
